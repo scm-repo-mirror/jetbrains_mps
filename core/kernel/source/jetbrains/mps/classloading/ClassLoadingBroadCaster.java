b84d39981b78e40045eab53ebaa45b2c5a0f1923
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2019-04-22 15:19:20 +0200

    Avoid improper progress monitor subTask()/advance() warning

--- core/kernel/source/jetbrains/mps/classloading/ClassLoadingBroadCaster.java
+++ core/kernel/source/jetbrains/mps/classloading/ClassLoadingBroadCaster.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2018 JetBrains s.r.o.
+ * Copyright 2003-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -71,7 +71,7 @@ public class ClassLoadingBroadCaster {
     myLoadedModules.removeAll(modulesToUnload);
 
     try {
-      monitor.start("Broadcasting Unload Events", myDeployListeners.size());
+      monitor.start("Broadcasting Unload Events", 2 * myDeployListeners.size());
       DisposeSession session = myDisposer.createSession(modulesToUnload);
       ResourceTrackerCallback trackerCallback = session.getTrackerCallback();
       for (DeployListener listener : myDeployListeners) {
