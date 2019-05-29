50456dace0d9497d18c665262ea27d92214cd324
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2019-05-29 18:30:55 +0200

    UsagesTree: avoid duplicates when top categories are hidden
    When the same result node is reported under more than one category, and the categories are not displayed, number of identical results is the same as number of categories. Merge result children prior to creating UI elements for them

--- workbench/mps-ui/source/jetbrains/mps/ide/findusages/view/UsagesTree.java
+++ workbench/mps-ui/source/jetbrains/mps/ide/findusages/view/UsagesTree.java
@@ -39,17 +39,18 @@ import jetbrains.mps.smodel.ModelReadRunnable;
 import jetbrains.mps.util.annotation.ToRemove;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.coverage.gnu.trove.THashSet;
 
 import javax.swing.Icon;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 import java.awt.Font;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -242,13 +243,11 @@ public class UsagesTree extends MPSTree {
   //this is not recursive
   //use only for top-level nodes
   private UsagesTreeNode buildTree(BaseNodeData root, HashSet<PathItemRole> nodeCategories) {
-    List<UsagesTreeNode> children = buildSubtreeStructure(root, nodeCategories);
-    assert children.size() == 1;
-
-    UsagesTreeNode child = children.get(0);
+    UsagesTreeNode child = newTreeNode(root);
+    buildSubtreeStructure(child, root, nodeCategories);
 
     buildCounters(child);
-    sortByCaption(children, new Comparator<UsagesTreeNode>() {
+    sortByCaption(child.getChildren(), new Comparator<UsagesTreeNode>() {
       private boolean isIgnored(UsagesTreeNode node) {
         // need to keep order of non-root nodes as they seen in an editor (see MPS-6113)
         BaseNodeData data = node.getUsageData();
@@ -277,6 +276,7 @@ public class UsagesTree extends MPSTree {
     return child;
   }
 
+  // XXX here we imply children list is actual collection of tree elements, not a copy.
   private static void sortByCaption(List<? extends UsagesTreeNode> children, Comparator<UsagesTreeNode> comparator) {
     children.sort(comparator);
     for (UsagesTreeNode child : children) {
@@ -284,35 +284,49 @@ public class UsagesTree extends MPSTree {
     }
   }
 
-  private List<UsagesTreeNode> buildSubtreeStructure(BaseNodeData data, HashSet<PathItemRole> nodeCategories) {
-    // FIXME introduce more effective way to add children in case data isPathTai() == true (add child tree elements right away, w/o intermediate array)
-    List<UsagesTreeNode> children = new ArrayList<>();
-    data.children().map(child -> buildSubtreeStructure(child, nodeCategories)).forEach(children::addAll);
-
-    if (nodeCategories.contains(data.getRole()) || data.isPathTail()) {
-      UsagesTreeNode node = new UsagesTreeNode(data);
-      Icon icon = data.getIcon(() -> myProject.getRepository());
-      if (data.isResultNode()) {
-        final LayeredIcon result = new LayeredIcon(2);
-        result.setIcon(icon, 0);
-        result.setIcon(Nodes.UsagesResultOverlay, 1);
-        node.setIcon(result);
-      } else {
-        node.setIcon(icon);
-      }
-
-      // XXX this code vividly illustrates the issue with BaseNodeData, which duplicates stuff otherwise kept in MPSTreeNode
-      //     alternatively, it shows there's no need in MPSTreeNode, BaseNodeData could serve as tree model.
-      node.setText(data.getCaption());
-      node.setAdditionalText(data.getAdditionalInfo());
-
-      for (UsagesTreeNode child : children) {
-        node.add(child);
-      }
-      children.clear();
-      children.add(node);
+  private UsagesTreeNode newTreeNode(BaseNodeData data) {
+    UsagesTreeNode node = new UsagesTreeNode(data);
+    Icon icon = data.getIcon(() -> myProject.getRepository());
+    if (data.isResultNode()) {
+      final LayeredIcon result = new LayeredIcon(2);
+      result.setIcon(icon, 0);
+      result.setIcon(Nodes.UsagesResultOverlay, 1);
+      node.setIcon(result);
+    } else {
+      node.setIcon(icon);
     }
-    return children;
+
+    // XXX this code vividly illustrates the issue with BaseNodeData, which duplicates stuff otherwise kept in MPSTreeNode
+    //     alternatively, it shows there's no need in MPSTreeNode, BaseNodeData could serve as tree model.
+    node.setText(data.getCaption());
+    node.setAdditionalText(data.getAdditionalInfo());
+    return node;
+  }
+
+  private void buildSubtreeStructure(UsagesTreeNode parent, BaseNodeData data, HashSet<PathItemRole> nodeCategories) {
+    LinkedList<BaseNodeData> childrenQueue = new LinkedList<>();
+    data.children().forEach(childrenQueue::add);
+    int i = 0;
+    while (i < childrenQueue.size()) {
+      final BaseNodeData child = childrenQueue.get(i);
+      if (nodeCategories.contains(child.getRole()) || data.isPathTail()) {
+        // regular, visible child element, just move on to the next one
+        i++;
+      } else {
+        // we are not going to visualize this one, replace with its children
+        childrenQueue.remove(i); // don't change i value!
+        child.children().forEach(childrenQueue::add);
+      }
+    }
+    THashSet<Object> seenChild = new THashSet<>();
+    for(BaseNodeData c : childrenQueue) {
+      if (!seenChild.add(c.getIdObject())) {
+        continue;
+      }
+      final UsagesTreeNode treeChild = newTreeNode(c);
+      buildSubtreeStructure(treeChild, c, nodeCategories);
+      parent.add(treeChild);
+    }
   }
 
   private int buildCounters(UsagesTreeNode root) {
