7fc66cf113290557d73aff4b71cc088d53da6893
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2019-10-10 12:50:29 +0200

    CCE ProjectFrameHelper to IdeFrameImpl
    and another one, as IdeFrame is not necessarily awt.Frame as MPS_Frame key manifests.

--- workbench/mps-platform/source/jetbrains/mps/ide/dataExtraction/FrameRule.java
+++ workbench/mps-platform/source/jetbrains/mps/ide/dataExtraction/FrameRule.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2016 JetBrains s.r.o.
+ * Copyright 2003-2019 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,29 +19,19 @@ import com.intellij.ide.impl.dataRules.GetDataRule;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
-import com.intellij.openapi.wm.impl.IdeFrameImpl;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class FrameRule implements GetDataRule {
-  private static final Logger LOG = LogManager.getLogger(FrameRule.class);
+import javax.swing.JFrame;
 
+public class FrameRule implements GetDataRule {
   @Override
   @Nullable
-  public Object getData(DataProvider dataProvider) {
+  public JFrame getData(@NotNull DataProvider dataProvider) {
     Project project = CommonDataKeys.PROJECT.getData(dataProvider);
     if (project == null) {
-      IdeFrame[] frames = WindowManager.getInstance().getAllProjectFrames();
-      for (IdeFrame frame : frames) {
-        if (((IdeFrameImpl) frame).isActive()) {
-          return frame;
-        }
-      }
-      LOG.warn("Active frames have not been found, falling back to the first one");
-      return frames.length == 0 ? null : frames[0];
+      return WindowManager.getInstance().findVisibleFrame();
     }
     return WindowManager.getInstance().getFrame(project);
   }
