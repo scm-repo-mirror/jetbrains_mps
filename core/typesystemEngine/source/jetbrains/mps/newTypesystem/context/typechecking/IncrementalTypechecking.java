69df419117257b0e97305a97c61684f551b2eb29
Victor.Matchenko <Victor.Matchenko@jetbrains.com>
2020-05-26 14:24:47 +0200

    platform 202.4357.1: fix of IncrementalTypechecking NPE
    ea13b08 Trove4j updated (null-check added to THashSet::add)
    Previously adding null to THashSet was possible and it was processed in TypeSystemComponent#addDependentNodesTypeSystem method.
    To avoid using knowledge of called method logic will pass empty Set in null case scenario.

--- core/typesystemEngine/source/jetbrains/mps/newTypesystem/context/typechecking/IncrementalTypechecking.java
+++ core/typesystemEngine/source/jetbrains/mps/newTypesystem/context/typechecking/IncrementalTypechecking.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2018 JetBrains s.r.o.
+ * Copyright 2003-2020 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -209,8 +209,9 @@ public class IncrementalTypechecking extends ReportingTypechecking<State, TypeSy
       return;
     }
 
-    Set<SNode> hashSet = new THashSet<>(1);
-    hashSet.add(myNodeTypeAccess.peekNode());
+    Set<SNode> hashSet = myNodeTypeAccess.peekNode() != null ?
+                         Collections.singleton(myNodeTypeAccess.peekNode()) :
+                         Collections.emptySet();
     getTypecheckingComponent().addDependentNodesTypeSystem(node, hashSet, typeAffected);
   }
 
