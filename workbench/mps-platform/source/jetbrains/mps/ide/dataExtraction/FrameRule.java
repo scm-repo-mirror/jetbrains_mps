071ecaa1ebd28f9168cbe97738f5a3e9eb9df16a
apyshkin <aapyshkin@gmail.com>
2020-11-18 14:48:25 +0300

    FrameRule is also fixed in a way
    There was a serious potential problem due to the fact that
    WindowManager.getInstance().getVisibleFrame() looks for the first
    visible frame which is not necessarily the frame on the top.
    So the returned frame might be not the one you are looking for.
    The fix uses another API which proved working better on my computer at
    least.
    Adding a warning to get whether we are in potential danger in 203.

--- workbench/mps-platform/source/jetbrains/mps/ide/dataExtraction/FrameRule.java
+++ workbench/mps-platform/source/jetbrains/mps/ide/dataExtraction/FrameRule.java
@@ -19,24 +19,53 @@ import com.intellij.ide.impl.dataRules.GetDataRule;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.wm.IdeFrame;
 import com.intellij.openapi.wm.WindowManager;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.swing.FocusManager;
 import javax.swing.JFrame;
+import java.awt.Window;
 
 public class FrameRule implements GetDataRule {
+  private static final Logger LOG = LogManager.getLogger(FrameRule.class);
+
   @Override
   @Nullable
   public JFrame getData(@NotNull DataProvider dataProvider) {
-    Project project = CommonDataKeys.PROJECT.getData(dataProvider);
+    Project project = determineProject(dataProvider);
     if (project == null) {
       // XXX this part could be quite dangerous, if anyone uses JFrame for anything but JFrame
       //     e.g. LocationRule used to obtain MPSProject from JFrame, and ruined concept behind
       //     IDEA's DataProvider hierarchy poll, by letting an incapable dataProvider to answer inquiries  it
       //     wasn't supposed to cover.
+      // fixme this branch in 202/203 leads to not currently active frame and I do not know of the correct api
+      //       #deduceFromActive window is the correct approach but there is no Frame there so I am lost
+      LOG.warn("incorrect frame might be returned");
       return WindowManager.getInstance().findVisibleFrame();
     }
     return WindowManager.getInstance().getFrame(project);
+  }
+
+  @Nullable
+  private Project determineProject(@NotNull DataProvider dataProvider) {
+    Project project = CommonDataKeys.PROJECT.getData(dataProvider);
+    return project != null ? project
+                           : deduceFromActiveWindow();
+  }
+
+  @Nullable
+  private Project deduceFromActiveWindow() {
+    Project project = null;
+    Window activeWindow = FocusManager.getCurrentManager().getActiveWindow();
+    if (activeWindow instanceof IdeFrame) {
+      project = ((IdeFrame) activeWindow).getProject();
+    } else {
+      LOG.debug("Active frames have not been found");
+    }
+    return project;
   }
 }
