048bae51879e9022905b56aaa497b7b318d3ce13
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2021-07-05 13:41:21 +0200

    platform 212.4416.7: fix MPSHeadlessPlatformStarter compilation
    MainRunner replaced with StartupUtil

--- workbench/mps-platform/source/jetbrains/mps/tool/environment/MPSHeadlessPlatformStarter.java
+++ workbench/mps-platform/source/jetbrains/mps/tool/environment/MPSHeadlessPlatformStarter.java
@@ -1,22 +1,10 @@
 /*
- * Copyright 2003-2020 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.tool.environment;
 
-import com.intellij.ide.plugins.MainRunner;
 import com.intellij.idea.MainImpl;
+import com.intellij.idea.StartupUtil;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ApplicationStarter;
@@ -58,9 +46,14 @@ public final class MPSHeadlessPlatformStarter implements ApplicationStarter {
      */
     @NotNull
     /*package*/ Application createApp() {
-      MainRunner.start(MAIN_CLASS.getName(),
-                       new String[]{MPSHeadlessPlatformStarter.CMD_NAME},
-                       new LinkedHashMap<>());
+      try {
+        StartupUtil.start(MAIN_CLASS.getName(),
+                          new String[]{MPSHeadlessPlatformStarter.CMD_NAME},
+                          new LinkedHashMap<>());
+      } catch (Exception e) {
+        throw new RuntimeException("FAILED TO START CMDLINE IJ", e);
+      }
+
       myLock.lock();
       try {
         myInitializedCondition.await(100, TimeUnit.SECONDS);
