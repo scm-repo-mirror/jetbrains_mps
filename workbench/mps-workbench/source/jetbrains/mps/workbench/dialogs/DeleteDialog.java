ca8612ef4131194bd1a2ebae29a6d0fc9d941d86
Victor Matchenko <Victor.Matchenko@jetbrains.com>
2021-07-05 14:31:58 +0200

    platform 212.4416.7: fix DeleteDialog compilation
    _init method required additional parameter

--- workbench/mps-workbench/source/jetbrains/mps/workbench/dialogs/DeleteDialog.java
+++ workbench/mps-workbench/source/jetbrains/mps/workbench/dialogs/DeleteDialog.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.workbench.dialogs;
 
@@ -53,7 +41,7 @@ public class DeleteDialog extends MessageDialog {
   public DeleteDialog(Project project, String title, String message, DeleteOption... options) {
     myDeleteOptions = options;
 
-    _init(title,message, new String[]{"Delete", Messages.CANCEL_BUTTON}, 0, 0, Messages.getQuestionIcon(), null);
+    _init(title, message, new String[]{"Delete", Messages.CANCEL_BUTTON}, 0, 0, Messages.getQuestionIcon(), null, null);
   }
 
   @Nullable
