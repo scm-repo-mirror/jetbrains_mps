a618e332ad161e4b53f161be8ce9ca7893f9ac3a
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2022-01-20 16:44:37 +0100

    MPS-34167 Exception in Find Text in Project action
    See FindPopupPanel, github commit 1243f1416, origin 82e00081bd47. Registry key removed.

--- workbench/mps-ui/source/jetbrains/mps/ide/ui/FindTextInModelDialog.java
+++ workbench/mps-ui/source/jetbrains/mps/ide/ui/FindTextInModelDialog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2021 JetBrains s.r.o.
+ * Copyright 2003-2022 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -131,7 +131,7 @@ public class FindTextInModelDialog extends DialogWrapper {
     myProject = mpsProject;
     mySearchSchedule = new Alarm(getDisposable());
     setOKButtonText("Open in Find Usages");
-    setUndecorated(!Registry.is("ide.find.as.popup.decorated"));
+    setUndecorated(true);
     init();
     final Window window = WindowManager.getInstance().suggestParentWindow(mpsProject.getProject());
     Component parent = UIUtil.findUltimateParent(window);
