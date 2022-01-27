81a4d44f51a73b8cfff4799870409f130ebe9b6c
mikhail.volkov <mikhail.volkov@jetbrains.com>
2022-01-27 14:11:35 +0300

    MPS-34141 enable Favorites tool.
    treat models and modules as logical units rather than
    files when adding tree nodes to Favorites.

--- workbench/mps-workbench/source/jetbrains/mps/ide/projectPane/favorites/providers/MPSFavoriteModelProvider.java
+++ workbench/mps-workbench/source/jetbrains/mps/ide/projectPane/favorites/providers/MPSFavoriteModelProvider.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2003-2021 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.ide.projectPane.favorites.providers;
 
@@ -24,13 +12,14 @@ import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import jetbrains.mps.ide.projectPane.favorites.nodes.MPSFavoriteModel;
+import jetbrains.mps.ide.ui.tree.smodel.SModelTreeNode;
 import jetbrains.mps.workbench.MPSDataKeys;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.mps.openapi.model.SModel;
 import org.jetbrains.mps.openapi.model.SModelReference;
 import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
 
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -40,14 +29,18 @@ public class MPSFavoriteModelProvider extends FavoriteNodeProvider {
   @Override
   public Collection<AbstractTreeNode<?>> getFavoriteNodes(DataContext context, @NotNull ViewSettings viewSettings) {
     Project project = CommonDataKeys.PROJECT.getData(context);
-    List<SModel> models = MPSDataKeys.MODELS.getData(context);
+    List<TreeNode> nodes = MPSDataKeys.TREE_NODES.getData(context);
 
-    if (models == null) {
+    if (nodes == null) {
       return null;
     }
     Collection<AbstractTreeNode<?>> result = new ArrayList<>();
-    for (SModel model : models) {
-      result.add(new MPSFavoriteModel(project, model.getReference(), viewSettings));
+    for (TreeNode treeNode : nodes) {
+      if (!(treeNode instanceof SModelTreeNode)) {
+        continue;
+      }
+      SModelTreeNode sModelTreeNode = (SModelTreeNode) treeNode;
+      result.add(new MPSFavoriteModel(project, sModelTreeNode.getModel().getReference(), viewSettings));
     }
 
     return result.isEmpty() ? null : result;
--- workbench/mps-workbench/source/jetbrains/mps/ide/projectPane/favorites/providers/MPSFavoriteNodeProvider.java
+++ workbench/mps-workbench/source/jetbrains/mps/ide/projectPane/favorites/providers/MPSFavoriteNodeProvider.java
@@ -1,17 +1,5 @@
 /*
- * Copyright 2003-2021 JetBrains s.r.o.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
  */
 package jetbrains.mps.ide.projectPane.favorites.providers;
 
@@ -24,13 +12,14 @@ import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import jetbrains.mps.ide.projectPane.favorites.nodes.MPSFavoriteNode;
+import jetbrains.mps.ide.ui.tree.MPSTreeNodeEx;
 import jetbrains.mps.workbench.MPSDataKeys;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import org.jetbrains.mps.openapi.model.SNode;
 import org.jetbrains.mps.openapi.model.SNodeReference;
 import org.jetbrains.mps.openapi.persistence.PersistenceFacade;
 
+import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -41,15 +30,21 @@ public class MPSFavoriteNodeProvider extends FavoriteNodeProvider {
   @Override
   public Collection<AbstractTreeNode<?>> getFavoriteNodes(DataContext context, @NotNull ViewSettings viewSettings) {
     Project project = CommonDataKeys.PROJECT.getData(context);
-    List<SNode> nodes = MPSDataKeys.NODES.getData(context);
+    List<TreeNode> nodes = MPSDataKeys.TREE_NODES.getData(context);
 
     if (nodes == null) {
       return null;
     }
     Collection<AbstractTreeNode<?>> result = new ArrayList<>();
-
-    for (SNode node : nodes) {
-      result.add(new MPSFavoriteNode(project, node.getReference(), viewSettings));
+    for (TreeNode node : nodes) {
+      if (!(node instanceof MPSTreeNodeEx)) {
+        continue;
+      }
+      MPSTreeNodeEx mpsTreeNode = (MPSTreeNodeEx) node;
+      SNodeReference nodeReference = mpsTreeNode.getNodePointer();
+      if (nodeReference != null) {
+        result.add(new MPSFavoriteNode(project, nodeReference, viewSettings));
+      }
     }
 
     return result.isEmpty() ? null : result;
