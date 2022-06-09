e25f43bbaa60d05718163862ad2c7bde144730d8
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2022-06-09 18:06:28 +0200

    GeneratorCacheComponent as service, not app component.

--- workbench/mps-workbench/source/jetbrains/mps/ide/generator/GeneratorCacheComponent.java
+++ workbench/mps-workbench/source/jetbrains/mps/ide/generator/GeneratorCacheComponent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2011 JetBrains s.r.o.
+ * Copyright 2003-2022 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,43 +17,32 @@ package jetbrains.mps.ide.generator;
 
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.PathManager;
-import com.intellij.openapi.components.ApplicationComponent;
 import jetbrains.mps.generator.GenerationCacheContainer;
 import jetbrains.mps.generator.GenerationCacheContainer.FileBasedGenerationCacheContainer;
-import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
 /**
  * Evgeny Gryaznov, Sep 21, 2010
  */
-public class GeneratorCacheComponent implements ApplicationComponent {
+public class GeneratorCacheComponent {
 
   private File generatorCaches;
 
   public static GeneratorCacheComponent getInstance() {
-    return ApplicationManager.getApplication().getComponent(GeneratorCacheComponent.class);
+    return ApplicationManager.getApplication().getService(GeneratorCacheComponent.class);
   }
 
-  @NotNull
-  @Override
-  public String getComponentName() {
-    return "Generator caches component";
-  }
-
-  @Override
   public void initComponent() {
-    generatorCaches = new File(PathManager.getSystemPath(), "mps-generator");
-    if(!generatorCaches.exists() && !generatorCaches.mkdirs()) {
-      generatorCaches = null;
-    }
-  }
-
-  @Override
-  public void disposeComponent() {
   }
 
   public GenerationCacheContainer getCache() {
+    if (generatorCaches == null) {
+      generatorCaches = new File(PathManager.getSystemPath(), "mps-generator");
+      if(!generatorCaches.exists() && !generatorCaches.mkdirs()) {
+        throw new IllegalStateException("Can't create cache dir +" + generatorCaches.getPath());
+      }
+    }
     return new FileBasedGenerationCacheContainer(generatorCaches);
   }
 }
