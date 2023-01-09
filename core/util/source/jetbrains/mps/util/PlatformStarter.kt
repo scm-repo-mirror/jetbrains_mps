80b3dc3e77ea845cf30c2f4d45d231a13c94b3a0
Clément de La Bourdonnaye <cle.bourdonnaye@gmail.com>
2023-01-09 10:17:44 +0100

    PlatformStarter.kt: use coroutineScope instead of thread + documentation

--- core/util/source/jetbrains/mps/util/PlatformStarter.kt
+++ core/util/source/jetbrains/mps/util/PlatformStarter.kt
@@ -9,36 +9,30 @@ import com.intellij.idea.startApplication
 import kotlinx.coroutines.*
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
-import kotlin.coroutines.AbstractCoroutineContextElement
-import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.*
 
 class PlatformStarter {
     companion object {
-        private const val CMD_NAME = "mps-inspect" // important for that to end with 'inspect' see com.intellij.idea.Main
+        private const val CMD_NAME = "mps-inspect" // ending with 'inspect' allows to set headless, see com.intellij.idea.Main
         @JvmStatic
         fun startApplication() =
-            doStartApplication()
+            runBlocking { doStartApplication() }
 
         @JvmStatic
         fun startApplicationAsync() =
-            Thread(::doStartApplication).start()
+            // The job returned here may be used for cancellation
+            GlobalScope.launch { doStartApplication() }
 
-        private fun doStartApplication() {
+        private suspend fun doStartApplication() {
             val appInitPreparationActivity = StartUpMeasurer.startActivity("app initialization preparation")
 
-            // TODO isHeadless: set either with java.awt.headless flag in system property, or with arg of less than 20 chars ending by "inspect"
-            //  see com.intellij.idea.AppMode.isHeadless(java.util.List<java.lang.String>)
-
-            // TODO this should replaces setFlagsAgain of anterior StartupUtil
+            // This replaces setFlagsAgain of anterior StartupUtil
             AppMode.setFlags(listOf(CMD_NAME))
 
-            // TODO this runBlocking could be replaced by run {} if we want it async
-            runBlocking(rootTask()) {
+            coroutineScope {
                 // Loads the app starter implementation from IDEA
                 val appStarter = async(CoroutineName("starter creation") + Dispatchers.Default) {
-                    // TODO not sure why idea load this class like this (if we want to use the same we have no choice since it is internal, hence not in our
-                    //  scope), perhaps we could make PlatformStarter some AppStarter and return it there instead (MainImpl has only two simple calls, we need
-                    //  to know if both are necessary in our case)
+                    // MainImpl is internal, we cannot access it easily
                     val aClass = AppStarter::class.java.classLoader.loadClass("com.intellij.idea.MainImpl")
                     MethodHandles.lookup().findConstructor(aClass, MethodType.methodType(Void.TYPE)).invoke() as AppStarter
                 }
@@ -46,20 +40,13 @@ class PlatformStarter {
                 // TODO is the StartupAbortedExceptionHandler (copied below) the way we want to handle errors here?
                 withContext(Dispatchers.Default + StartupAbortedExceptionHandler()) {
                     StartUpMeasurer.appInitPreparationActivity = appInitPreparationActivity
-                    // TODO i'm quite unsure what this CMD_NAME purpose is, if headless is its only purpose we can perhaps remove it as the setFlags above does that already
-                    startApplication(listOf<String>(CMD_NAME), appStarter, this@runBlocking, Thread.currentThread())
+                    // CMD_NAME here has two purposes: set headless to true (ends in "inspect") and run our MPSHeadlessPlatformStarter starter (which is
+                    // declared using the same name)
+                    startApplication(listOf<String>(CMD_NAME), appStarter, this, Thread.currentThread())
                 }
 
                 awaitCancellation()
             }
-            /*
-            StartupUtil.start(MAIN_CLASS.getName(),
-                              true, // Since 2022.1, IDEA takes isHeadless explicitly (although still uses Main.isHeadless())
-                              true, // XXX if start() fails, Main.showMessage reports an error, and needs to detect
-                                    // isHeadless || isCommandLine from Main e.g. not to show message dialog on TC
-                              new String[]{MPSHeadlessPlatformStarter.CMD_NAME},
-                              new LinkedHashMap<>());
-             */
         }
     }
 
