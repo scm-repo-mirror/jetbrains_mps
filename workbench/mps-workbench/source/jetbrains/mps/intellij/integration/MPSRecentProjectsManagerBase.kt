0a3162bcccfd722858ccc99a81f400b1a85b73a8
Vaclav <vaclav.pech@gmail.com>
2023-02-03 13:56:37 +0100

    MPS-35253 - allow null parameters and log, if they happen to be called

--- workbench/mps-workbench/source/jetbrains/mps/intellij/integration/MPSRecentProjectsManagerBase.kt
+++ workbench/mps-workbench/source/jetbrains/mps/intellij/integration/MPSRecentProjectsManagerBase.kt
@@ -10,19 +10,34 @@ import java.nio.file.Path
 
 private val LOG = logger<MPSRecentProjectsManagerBase>()
 
-suspend fun openProject(projectFile: Path, options: OpenProjectTask, superFun: suspend (projectFile: Path, options: OpenProjectTask) -> Project?): Project? {
+suspend fun openProject(projectFile: Path?, options: OpenProjectTask?, superFun: (suspend (projectFile: Path, options: OpenProjectTask) -> Project?)?): Project? {
+    //It seems that coroutines call this method by mistake with all null parameters values, if the method declares any of the parameters as mandatory.
+    // This causes the problem of MPS-35253.
+    if (projectFile == null) {
+        LOG.error("MPSRecentProjectsManagerBase called with null path parameter", IllegalArgumentException("Unexpected null argument"))
+        return null
+    }
+    if (options == null) {
+        LOG.error("MPSRecentProjectsManagerBase called with null options parameter", IllegalArgumentException("Unexpected null argument"))
+        return null
+    }
+    if (superFun == null) {
+        LOG.error("MPSRecentProjectsManagerBase called with null superFun parameter", IllegalArgumentException("Unexpected null argument"))
+        return null
+    }
     val localOptions = options
     //TODO Is this still needed?
 //        val localOptions = if (options.runConfigurators)
-            //OpenProjectTask(projectToClose = options.projectToClose, forceOpenInNewFrame = options.forceOpenInNewFrame)
+    //OpenProjectTask(projectToClose = options.projectToClose, forceOpenInNewFrame = options.forceOpenInNewFrame)
 //            options.copy(runConfigurators = false)
 //        else options
 
     try {
         val trusted = OpenMPSProjectTrustProjectHelperK().checkTrust(projectFile)
-        return if (trusted)
+
+        return if (trusted) {
             superFun(projectFile, localOptions)
-        else {
+        } else {
             LOG.info("Project is not trusted -> return null")
             null
         }
