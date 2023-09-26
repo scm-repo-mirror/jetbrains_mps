2e5d9544037704f8c9878d252cbe929312bdc9d1
tatiana.elfimova <tatiana.elfimova@jetbrains.com>
2023-09-26 16:01:22 +0200

    Introduce `PopupBridge` class to perform an action when AbstractPopup is available

---
+++ editor/editor-runtime/source/jetbrains/mps/nodeEditor/PopupBridge.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2000-2023 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+ */
+package jetbrains.mps.nodeEditor;
+
+import com.intellij.ui.popup.AbstractPopup;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Consumer;
+
+class PopupBridge {
+  private AbstractPopup myPopup;
+  private Consumer<? super AbstractPopup> myConsumer;
+
+  public PopupBridge() {
+  }
+
+  public void setPopup(@NotNull AbstractPopup popup) {
+    assert myPopup == null;
+    myPopup = popup;
+    if (myConsumer != null) {
+      myConsumer.accept(myPopup);
+      myConsumer = null;
+    }
+  }
+
+  @Nullable
+  AbstractPopup getPopup() {
+    return myPopup;
+  }
+
+  public void performWhenAvailable(@NotNull Consumer<? super AbstractPopup> consumer) {
+    if (myPopup == null) {
+      myConsumer = consumer;
+    } else {
+      consumer.accept(myPopup);
+    }
+  }
+
+}
