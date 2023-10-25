b72625b9f4aa7befd59b43dd63090170f9f1348d
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-10-25 11:48:47 +0300

    MPS-36385 Exception initializing components on project switch
    account for <projectListener> prompt lifecycle

--- workbench/mps-editor/source/jetbrains/mps/ide/editor/icons/NodeIconUpdater.java
+++ workbench/mps-editor/source/jetbrains/mps/ide/editor/icons/NodeIconUpdater.java
@@ -15,9 +15,11 @@
  */
 package jetbrains.mps.ide.editor.icons;
 
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import jetbrains.mps.logging.Logger;
 import jetbrains.mps.nodefs.NodeFileEventListener;
 
 import java.util.Collection;
@@ -32,16 +34,31 @@ public class NodeIconUpdater implements NodeFileEventListener {
 
   public NodeIconUpdater(Project project) {
     myProject = project;
+    // FWIW, there's no reason to implement Disposable for projectListener; it is removed automatically w/o notification (as of this writing, 23.2)
   }
 
   @Override
   public void changed(Collection<VirtualFile> vf) {
+    if (!myProject.isInitialized() || !myProject.isOpen()) {
+      // see beforeDelete()
+      return;
+    }
     final FileEditorManager fm = FileEditorManager.getInstance(myProject);
     vf.forEach(fm::updateFilePresentation);
   }
 
   @Override
   public void beforeDelete(Collection<VirtualFile> vf) {
+    if (!myProject.isInitialized() || !myProject.isOpen()) {
+      // with this listener being registered with <projectListener>, it may get initialized and receive notifications in inappropriate moment of time:
+      // There's project P1 open, and users open P2 in the same window.
+      // P1 closes and sends node file "delete" events. Closing means subscribers of its MB, including NodeIconUpdater(P1), get removed and don't receive
+      // notifications. At the same moment, P2 starts to initialize, and NodeIconUpdater(P2) is there as a projectListener, receiving notifications for a
+      // new MB connection. The project, P2, is not initialized at this moment, there's no FileEditorManager instance for it, and MPS fails with MPS-36385.
+      // FTR, before NodeIconUpdater became projectListener, it used to be ProjectComponent with another initialization sequence. I believe NodeIconUpdater(P2)
+      // was not initialized the moment NodeVirtualFileSystem(P1) dispatched "deleted" events (not all (any?) ProjectComponents got initialized by that time)
+      return;
+    }
     final FileEditorManager fm = FileEditorManager.getInstance(myProject);
     vf.forEach(fm::closeFile);
   }
