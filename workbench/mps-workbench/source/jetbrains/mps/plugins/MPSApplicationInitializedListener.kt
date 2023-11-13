5b3a7fda3a4505218cb9e0f2be2fc80c3ef2c184
Maros Sandor <maros.sandor@jetbrains.com>
2023-11-14 00:01:19 +0100

    23.3 platform: Workaround for early usage of read action

--- workbench/mps-workbench/source/jetbrains/mps/plugins/MPSApplicationInitializedListener.kt
+++ workbench/mps-workbench/source/jetbrains/mps/plugins/MPSApplicationInitializedListener.kt
@@ -1,14 +1,20 @@
 package jetbrains.mps.plugins
 
+import com.intellij.diagnostic.LoadingState
 import com.intellij.ide.ApplicationInitializedListener
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 @Suppress("UnstableApiUsage")
 class MPSApplicationInitializedListener : ApplicationInitializedListener {
 
     override suspend fun execute(asyncScope: CoroutineScope) {
         asyncScope.launch {
+            while (!LoadingState.COMPONENTS_LOADED.isOccurred) {
+                delay(1.milliseconds)
+            }
             PluginLoaderRegistry.getInstance().signalAppInitialized()
         }
     }
