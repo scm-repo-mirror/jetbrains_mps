36e88fee453b13725a5ce6110569bd79ac51ae16
Artem Tikhomirov <tikhomirov.artem@gmail.com>
2023-12-01 11:50:50 +0200

    SNodeAccessUtil.hasProperty: do not read property twice
    Since 7b3d85b4 (2015) we dispatch both NodeReadAccessCasterInEditor and NodeReadEventsCaster property read notifications, single hasProperty is enough to let either listener know about the access.

--- core/kernel/source/jetbrains/mps/smodel/SNodeAccessUtilImpl.java
+++ core/kernel/source/jetbrains/mps/smodel/SNodeAccessUtilImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2022 JetBrains s.r.o.
+ * Copyright 2003-2023 JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -48,9 +48,7 @@ public class SNodeAccessUtilImpl extends SNodeAccessUtil {
 
   @Override
   protected boolean hasPropertyImpl(org.jetbrains.mps.openapi.model.SNode node, SProperty property) {
-    node.hasProperty(property); //todo this is to invoke corresponding read access. try to remove it by merging 2 types of access
-    String property_internal = node.getProperty(property);
-    return !SModelUtil_new.isEmptyPropertyValue(property_internal);
+    return node.hasProperty(property);
   }
 
   @Override
