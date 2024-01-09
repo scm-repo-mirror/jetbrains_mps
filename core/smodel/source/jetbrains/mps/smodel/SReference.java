scm-repo-mirror/jetbrains_mps

commit 50dc70f22ee74c1e201b58d477e52f9f35c61e20

SReference: equals+hashCode
To deal with node.getReference(linkA).equals(node.getReference(linkA)) and the fact SReference is just a presentation object of a true AssociationData.

core/smodel/source/jetbrains/mps/smodel/SReference.java
@@ -30,6 +30,7 @@ import org.jetbrains.mps.openapi.model.SNode;
 import org.jetbrains.mps.openapi.model.SNodeId;
 import org.jetbrains.mps.openapi.model.SNodeReference;
 
+import java.util.Objects;
 import java.util.Set;
 
 public abstract class SReference implements org.jetbrains.mps.openapi.model.SReference {
@@ -155,6 +156,22 @@ public abstract class SReference implements org.jetbrains.mps.openapi.model.SRef
   /*package*/ AssociationData getData() {
     // FIXME shall be abstract but might require change in MPS-extensions or mbeddr
     return null;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(getData()) + getSourceNode().hashCode()*31 + getLink().hashCode()*17;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (obj.getClass() != getClass()) {
+      return false;
+    }
+    final SReference other = (SReference) obj;
+    // XXX I didn't implement equals for AssociationData as it seems sufficient for now just to
+    //     rely on == (tests pass). However, one day we might need to look deeper into AssociationData equality.
+    return Objects.equals(getData(), other.getData()) && getSourceNode() == other.getSourceNode() && getLink().equals(other.getLink());
   }
 
   protected abstract SNode getTargetNode_internal(/*not null*/ ProblemReporter reporter);

core/smodel/source/jetbrains/mps/smodel/StaticReference.java
@@ -326,7 +326,7 @@ public final class StaticReference extends SReferenceBase {
       }
       // assert sourceModel != null;
       // convert 'young' reference to 'mature'
-      setData(new Transition(true).makeIndirect(d, StaticReference::getResolveInfo));
+      setData(new Transition(true ).makeIndirect(d, StaticReference::getResolveInfo));
       // FWIW, myImmatureTargetNode == null here
     } else {
       if (force && immatureTargetNode != null) {

core/kernel/source/jetbrains/mps/smodel/DynamicReference.java
@@ -127,6 +127,8 @@ public final class DynamicReference extends SReferenceBase {
 
     final Set<DynamicReference> currentRefs = currentlyResolved.get();
     final Set<DynamicReference> loggedRefs = currentlySourceNodeLogged.get();
+    // FIXME use of (this) works as long as equals/hashCode is right. Consider using another identity object
+    //       or come up with another mechanism to avoid stack overflow and reference resolution cycles
     if (currentRefs.contains(this)) {
       // loop detected!
       if (!loggedRefs.contains(this)) {

commit 30b446529252ae950fb5a81e9074021a89cb47b1

NPE from SReference.equals(null)
Defect introduced in 50dc70f2

core/smodel/source/jetbrains/mps/smodel/SReference.java
@@ -164,7 +164,7 @@ public abstract class SReference implements org.jetbrains.mps.openapi.model.SRef
 
   @Override
   public boolean equals(Object obj) {
-    if (obj.getClass() != getClass()) {
+    if (obj == null || obj.getClass() != getClass()) {
       return false;
     }
     final SReference other = (SReference) obj;

core/smodel/source/jetbrains/mps/smodel/StaticReference.java
@@ -339,7 +339,7 @@ public final class StaticReference extends SReferenceBase {
       }
       // assert sourceModel != null;
       // convert 'young' reference to 'mature'
-      setData(new Transition(true ).makeIndirect(d, StaticReference::getResolveInfo));
+      setData(new Transition(true).makeIndirect(d, StaticReference::getResolveInfo));
       // FWIW, myImmatureTargetNode == null here
     } else {
       if (force && immatureTargetNode != null) {