8b5749f442ff72e08d528baed108c9f441f06a95
Maros Sandor <maros.sandor@jetbrains.com>
2024-10-10 17:45:28 +0200

    24.3 platform: New project file introduced: product-workspace.xml

--- testbench/tests/jetbrains/mps/workbench/ProjectCreationTest.java
+++ testbench/tests/jetbrains/mps/workbench/ProjectCreationTest.java
@@ -71,6 +71,7 @@ public class ProjectCreationTest implements EnvironmentAware {
   private static final List<String> PROJECT_PROPERTIES_DIR_CONTENT = Arrays.asList(
       PROJECT_PROPERTIES_DIR + "/modules.xml",
       PROJECT_PROPERTIES_DIR + "/workspace.xml",
+      PROJECT_PROPERTIES_DIR + "/product-workspace.xml",
       PROJECT_PROPERTIES_DIR + "/migration.xml");
 
   private static final List<String> EMPTY_PROJECT_PATH_LIST_FB = Arrays.asList(
